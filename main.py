ef7242244a77ec063150db886bbbe05fabb2ab49
bobrov.da <dmr.bobrov@yandex.ru>
2023-05-23 00:01:58 +0300

    HomeWork isOk

---
+++ main.py
@@ -0,0 +1,40 @@
+# #Task 2
+
+# n = [int(i) for i in input()]
+
+# sum = sum(n)
+
+# print(sum)
+
+#--------------------------------------------------------
+
+
+# #Task 4
+
+# s = int(input()) // 3
+
+# print(s // 2, s*2, s//2)
+
+#--------------------------------------------------------
+
+
+# # Task 6
+
+# s = str(input())
+# sum1=int(s[0])+int(s[1])+int(s[2])
+# sum2=int(s[3])+int(s[4])+int(s[5])
+# if sum1==sum2:
+#   print('Счастливый')
+# else:
+#   print('Обычный')
+
+
+# # Task 8
+
+# n = int(input())
+# m = int(input())
+# k = int(input())
+# if k < n * m and ((k % n == 0) or (k % m == 0)):
+#     print('YES')
+# else:
+#     print('NO')
